24b4e803e4ced6245fc19d465cd1a91a150243aa
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-10-12 00:01:53 +0300

    Added drawing of some types

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import traceback
 
@@ -16,15 +17,15 @@ def register_launch_arguments():
 
 
 def create_png_path(path_to_dxf, output):
-    return os.path.join(output, "%s.png" % os.path.basename(path_to_dxf)[:-len("dxf")])
+    return os.path.join(output, "%s.png" % os.path.basename(path_to_dxf)[:-len(".dxf")])
 
 
-def wrapper_painting(input, output):
+def wrapper_painting(in_path, out_path, config):
     try:
-        paint(input, output)
-        print("Filename `%s`: OK" % input)
+        paint(in_path, out_path, config)
+        print("Filename `%s`: OK" % in_path)
     except:
-        print("Filename `%s`: FAILURE" % input)
+        print("Filename `%s`: FAILURE" % in_path)
         traceback.print_exc()
         return 1
     return 0
@@ -35,6 +36,8 @@ if __name__ == '__main__':
 
     input_path = args.input
     output_path = args.output
+    with open(args.cfgpath, 'r', encoding='utf-8') as cfg:
+        config = json.load(cfg)
 
     if not os.path.exists(input_path):
         print("Input path `%s` doesn't exist" % input_path)
@@ -45,17 +48,17 @@ if __name__ == '__main__':
     cnt_fail = 0
 
     if os.path.isfile(input_path) and input_path[-len("dxf"):].lower() == "dxf":
-        cnt_fail += wrapper_painting(input_path, create_png_path(input_path, output_path))
+        cnt_fail += wrapper_painting(input_path, create_png_path(input_path, output_path), config)
     elif not args.tree:
         files = os.listdir(input_path)
         for _ in filter(lambda x: x[-len("dxf"):].lower() == "dxf", files):
-            cnt_fail += wrapper_painting(os.path.join(input_path, _), create_png_path(_, output_path))
+            cnt_fail += wrapper_painting(os.path.join(input_path, _), create_png_path(_, output_path), config)
     else:
         tree = os.walk(input_path)
         for _ in tree:
             if len(_[1]) > 0:
                 continue
             for file in filter(lambda x: x[-len("dxf"):].lower() == "dxf", _[2]):
-                cnt_fail += wrapper_painting(os.path.join(_[0], file), create_png_path(file, output_path))
+                cnt_fail += wrapper_painting(os.path.join(_[0], file), create_png_path(file, output_path), config)
 
     print("Fail converting: %d" % cnt_fail)
--- painter.py
+++ painter.py
@@ -1,6 +1,87 @@
 import cv2
-import sdxf
+import ezdxf
+import numpy as np
 
 
-def paint(input, output):
-    print("Draw dxf in png")
+def get_color(entity, layer):
+    code = entity.dxf.color
+    if code == 256:
+        return ezdxf.tools.aci2rgb(layer.dxf.color)
+    return ezdxf.tools.aci2rgb(code)
+
+
+def draw_hatch(img, entity, layer):
+    color = get_color(entity, layer)
+    for poly_path in entity.paths.paths:
+        print(poly_path.path_type_flags)
+        polygon = np.array([vertex[:-1] for vertex in poly_path.vertices]).astype(int)
+        if poly_path.path_type_flags & 1 == 1:
+            cv2.fillPoly(img, [polygon], color)
+        else:
+            cv2.fillPoly(img, [polygon], (255, 255, 255))
+
+
+def draw_line(img, entity, layer):
+    color = get_color(entity, layer)
+    p1 = entity.dxf.start[:-1]
+    p2 = entity.dxf.end[:-1]
+    cv2.line(img, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])), color, 1)
+
+
+def draw_lwpolyline(img, entity, layer):
+    color = get_color(entity, layer)
+    polyline = []
+    a = np.array(entity.lwpoints.values).astype(int)
+    while len(a) > 0:
+        polyline.append((a[0], a[1]))
+        a = a[5:]
+    cv2.polylines(img, [np.array(polyline)], entity.closed, color, 1)
+
+
+def draw_arc(img, entity, layer):
+    color = get_color(entity, layer)
+    s = entity.dxf.start_angle * np.pi / 180
+    e = entity.dxf.end_angle * np.pi / 180
+    if s > e:
+        s -= 2 * np.pi
+    d = (e - s) / (int((e - s) * 180 / np.pi) + 1)
+    r = entity.dxf.radius
+    cx, cy = entity.dxf.center.xyz[:-1]
+    angles = np.arange(s, e + d / 2, d)
+    x = cx + r * np.cos(angles)
+    y = cy + r * np.sin(angles)
+    points = np.column_stack((x, y)).astype(int)
+    cv2.polylines(img, [points], abs(s - e) < 1e-9, color, 1)
+
+
+draw_map = {
+    'HATCH': draw_hatch,
+    'LINE': draw_line,
+    'LWPOLYLINE': draw_lwpolyline,
+    'ARC': draw_arc,
+}
+
+
+def paint(in_path, out_path, config):
+    doc = ezdxf.readfile(in_path)
+    extmax, extmin = doc.header['$EXTMAX'], doc.header['$EXTMIN']
+    xmin, ymin = np.floor(extmin[:-1]).astype(int)
+    xmax, ymax = np.ceil(extmax[:-1]).astype(int)
+    img = np.ones((ymax + ymin, xmax + xmin, 3)) * 255
+    msp = doc.modelspace()
+    layers = config.get('layers', [])
+    print(doc.layers.entries.keys())
+    for layer_name in layers:
+        if layer_name not in doc.layers:
+            continue
+        print(layer_name)
+        entities = msp.query('*[layer=="%s"]' % layer_name)
+        layer = doc.layers.get(layer_name)
+        for entity in entities:
+            print(entity.DXFTYPE)
+            if entity.DXFTYPE in draw_map:
+                draw_map[entity.DXFTYPE](img, entity, layer)
+
+    res, im_png = cv2.imencode('.png', cv2.flip(img, 0))
+    with open(out_path, 'wb') as f:
+        f.write(im_png.tobytes())
